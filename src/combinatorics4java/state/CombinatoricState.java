13dddd54f469506829a8591322fb0d0dad0cc8d8
RavenOfSerenity <RavenOfSerenity@gmail.com>
2014-03-08 20:34:52 +0100

    -Updated API to allow more complex states

--- src/combinatorics4java/state/CombinatoricState.java
+++ src/combinatorics4java/state/CombinatoricState.java
@@ -13,30 +13,60 @@ public abstract class CombinatoricState<E> implements Iterator<List<E>> {
 			throw new IllegalArgumentException("Invalid size specified");
 		}
 		this.state = new int[size];
+		this.init();
 	}
 
 	public boolean hasNext() {
 		return !this.atEnd;
 	}
 
-	protected void incrementState() {
+	private void incrementState() {
 		int index = state.length - 1;
-		while (index >= 0 && this.state[index] >= this.getSize(index) - 1) {
+		while (index >= 0 && this.state[index] >= this.getRadix(index) - 1
+				&& this.isValid(index)) {
 			this.state[index] = 0;
 			index--;
 		}
 		if (index >= 0) {
 			this.state[index]++;
+			this.fill(index);
 		} else {
 			this.atEnd = true;
 		}
 	}
 
-	public int getIndex(int position) {
-		return this.state[position];
+	/*
+	 * Optional method used to initialise the state
+	 */
+	protected void init() {
+
 	}
 
-	public int size() {
+	/*
+	 * Determine if at a given iteration the state is valid starting at the
+	 * given index
+	 */
+	protected boolean isValid(int fromPosition) {
+		return true;
+	}
+
+	/*
+	 * Optional method used to fill the state after the normal increment of the
+	 * radix starting at the given index
+	 */
+	protected void fill(int fromIndex) {
+
+	}
+
+	protected int getIndex(int index) {
+		return this.state[index];
+	}
+
+	protected void setIndex(int position, int value) {
+		this.state[position] = value;
+	}
+
+	protected int size() {
 		return this.state.length;
 	}
 
@@ -55,6 +85,6 @@ public abstract class CombinatoricState<E> implements Iterator<List<E>> {
 
 	protected abstract List<E> getNextCombination();
 
-	protected abstract int getSize(int position);
+	protected abstract int getRadix(int index);
 
 }
